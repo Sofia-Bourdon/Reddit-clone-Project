e497eee5fea6e1b4a74c077f115850cbf3ca876d
Sofia-Bourdon <mabourdonsofia@gmail.com>
2023-06-20 15:51:00 +0000

    adding html templates and creating a first view for the site

--- blog/models.py
+++ blog/models.py
@@ -30,7 +30,6 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS_CHOICES, default=0)
