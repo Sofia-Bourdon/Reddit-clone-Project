82538ab5c87ec14696de865826d083344231ff36
Sofia-Bourdon <mabourdonsofia@gmail.com>
2023-06-21 15:15:32 +0000

    building post detail page and style with css

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home')
+    path('', views.PostList.as_view(), name='home'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 
@@ -7,4 +7,9 @@ class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
-    paginate_by = 6
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    approved_comments = post.comments.filter(approved=True).order_by('created_on')
+    return render(request, 'post_detail.html', {'post': post, 'comments': approved_comments})
