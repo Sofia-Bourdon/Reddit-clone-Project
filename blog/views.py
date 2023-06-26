7e38105c0d52d1cb2bf2ad13440053ab0b61e6da
Sofia-Bourdon <mabourdonsofia@gmail.com>
2023-06-26 13:02:36 +0000

    creating the functionality for the upvote and downvote buttons

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
+from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import CommentForm
+
 
 class PostList(generic.ListView):
     model = Post
@@ -9,19 +11,24 @@ class PostList(generic.ListView):
     template_name = 'index.html'
 
 
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     approved_comments = post.comments.filter(approved=True).order_by('created_on')
+    form = CommentForm()
 
     if request.method == 'POST':
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.post = post
-            comment.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = CommentForm()
+        if 'upvote' in request.POST:
+            handle_upvote(request, post)
+        elif 'downvote' in request.POST:
+            handle_downvote(request, post)
+        else:
+            form = CommentForm(request.POST)
+            if form.is_valid():
+                comment = form.save(commit=False)
+                comment.post = post
+                comment.save()
+                return redirect('post_detail', pk=post.pk)
 
     return render(
         request,
@@ -29,9 +36,31 @@ def post_detail(request, pk):
         {
             'post': post,
             'comments': approved_comments,
-            'comment_form': form
+            'comment_form': form,
         },
     )
+
+
+def handle_upvote(request, post):
+    if request.user.is_authenticated:
+        if request.user in post.upvotes.all():
+            post.upvotes.remove(request.user)
+        else:
+            post.upvotes.add(request.user)
+            post.downvotes.remove(request.user)
+    else:
+        return redirect('login')
+
+
+def handle_downvote(request, post):
+    if request.user.is_authenticated:
+        if request.user in post.downvotes.all():
+            post.downvotes.remove(request.user)
+        else:
+            post.downvotes.add(request.user)
+            post.upvotes.remove(request.user)
+    else:
+        return redirect('login')
 
 
 def comment_post(request, slug):
