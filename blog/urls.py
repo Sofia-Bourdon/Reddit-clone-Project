c10e03d0a86ac7d25d83c5dfa55d0846c506a196
Sofia-Bourdon <>
2023-11-25 17:27:20 +0000

    added logic and basic template for user profile

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from . import views
 from django.urls import path
 
-
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('make_new_post/', views.make_new_post, name='make_new_post'),
+    path('profile/', views.profile, name='profile'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('<slug:slug>/', views.comment_post, name='comment_post'),
     path('r/<slug:slug>/', views.subreddit, name='subreddit'),
